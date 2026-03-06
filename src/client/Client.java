3cfdcebfa10a6d37d29d5814618153465eb8b75b
gCap-pixel <gcappellin9@gmail.com>
2026-03-06 13:21:54 +0100

    secondo prototipo di comunicazione

--- src/client/Client.java
+++ src/client/Client.java
@@ -11,6 +11,9 @@ public class Client {
     private String nomeServer = "localhost";
     private int port = 5000;
 
+    // Serve un pacchetto d'appoggio per conservare l'ultimo messaggio ricevuto
+    private DatagramPacket lastReceivedPacket;
+
     public Client(String nome) {
         this.nome = nome;
     }
@@ -18,9 +21,42 @@ public class Client {
     public void connetti() {
         try {
             this.socket = new DatagramSocket();
-            System.out.println("Client pronto per l'invio.");
+            System.out.println("Client pronto.");
         } catch (IOException e) {
-            System.err.print("errore nella connessione");
+            System.err.println("Errore nella connessione");
+        }
+    }
+
+    public void leggi() {
+        try {
+            byte[] buffer = new byte[1024]; // Spazio per i dati in entrata
+            lastReceivedPacket = new DatagramPacket(buffer, buffer.length);
+
+            socket.receive(lastReceivedPacket); // Bloccante
+
+            String messaggio = new String(lastReceivedPacket.getData(), 0, lastReceivedPacket.getLength());
+            System.out.println("Ricevuto: " + messaggio);
+        } catch (IOException e) {
+            System.err.println("Errore nella lettura");
+        }
+    }
+
+    public void scrivi() {
+        try {
+            if (lastReceivedPacket == null) return;
+
+            String risposta = "Messaggio ricevuto correttamente";
+            byte[] bufferRisposta = risposta.getBytes();
+
+            // Recupero info dal mittente dell'ultimo pacchetto
+            InetAddress targetAddress = lastReceivedPacket.getAddress();
+            int targetPort = lastReceivedPacket.getPort();
+
+            DatagramPacket responsePacket = new DatagramPacket(bufferRisposta, bufferRisposta.length, targetAddress, targetPort);
+            socket.send(responsePacket);
+            System.out.println("Risposta inviata.");
+        } catch (IOException e) {
+            System.err.println("Messaggio non inviato");
         }
     }
 
@@ -28,22 +64,22 @@ public class Client {
         try {
             byte[] buffer = messaggio.getBytes();
             InetAddress address = InetAddress.getByName(nomeServer);
-
-            // Crei il pacchetto con i dati, l'indirizzo e la porta
             DatagramPacket packet = new DatagramPacket(buffer, buffer.length, address, port);
-
-            // Invii il pacchetto
             socket.send(packet);
-            System.out.println("Messaggio inviato: " + messaggio);
         } catch (IOException e) {
-            System.err.print("errore nell'invio");
+            System.err.println("Errore nell'invio");
         }
     }
 
     public void chiudi() {
-        if (socket != null && !socket.isClosed()) {
-            socket.close();
-            System.out.println("Socket chiuso.");
+        if (socket != null) socket.close();
+    }
+
+    // Metodo per estrarre il testo dall'ultimo pacchetto ricevuto
+    public String getMessaggio() {
+        if (lastReceivedPacket != null) {
+            return new String(lastReceivedPacket.getData(), 0, lastReceivedPacket.getLength());
         }
+        return null;
     }
 }
--- src/client/ClientMain.java
+++ src/client/ClientMain.java
@@ -1,11 +1,16 @@
 package client;
 
 public class ClientMain {
+    private static String messaggioRicevuto = "i";
     public static void main(String[] args) {
         Client client = new Client("gab");
         client.connetti();
-        client.invia("Ciao dal client di " + client);
-
+        while(!"stop".equals(messaggioRicevuto)) {
+            client.scrivi();
+            client.invia("Ciao dal client gab");
+            client.leggi();
+            messaggioRicevuto = client.getMessaggio();
+        }
         client.chiudi();
     }
 }
